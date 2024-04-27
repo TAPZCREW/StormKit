8311eeb58d9a752158b5c0f1783821b34088582f
Arthur LAURENT <arthur.laurent4@gmail.com>
2024-04-28 01:29:02 +0200

    [Core] add Locked sync object

---
+++ examples/engine/sprites/xmake.lua
@@ -0,0 +1,30 @@
+target("sprites")
+do
+	set_kind("binary")
+	set_languages("cxxlatest", "clatest")
+
+	-- add_rules("@nzsl/compile.shaders")
+	add_rules("compile.shaders")
+	add_deps("stormkit-core", "stormkit-main", "stormkit-log", "stormkit-wsi", "stormkit-engine")
+
+	if is_mode("debug") then
+		add_defines("STORMKIT_BUILD_DEBUG")
+		add_defines("STORMKIT_ASSERT=1")
+		set_suffixname("-d")
+	else
+		add_defines("STORMKIT_ASSERT=0")
+	end
+
+  if is_plat("windows") then
+      add_ldflags("-Wl,/SUBSYSTEM:CONSOLE", {force = true})
+  end
+
+	add_files("src/*.cpp")
+	add_files("src/*.mpp")
+	add_files("shaders/*.nzsl")
+	if is_plat("windows") then
+		add_files("win32/*.manifest")
+	end
+
+	set_group("examples/stormkit-engine")
+end
