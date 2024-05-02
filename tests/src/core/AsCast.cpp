8e81420b724ac18085551f8ec1a4af995db53705
Arthur LAURENT <arthur.laurent4@gmail.com>
2024-05-02 03:09:27 +0200

    [Core] improve AsCast tests

--- tests/src/core/AsCast.cpp
+++ tests/src/core/AsCast.cpp
@@ -16,17 +16,17 @@ namespace {
     auto _ =
         test::TestSuite { "Core.AsCast",
                           {
-                              { "AsCast.VariantIs",
-                                [] {
+                              { "is<std::variant>",
+                                [] static noexcept {
                                     using Variant1 = std::variant<bool, int>;
 
                                     auto variant = Variant1 { false };
 
-                                    expects((is<bool>(variant)));
-                                    expects((not is<int>(variant)));
+                                    expects(is<bool>(variant));
+                                    expects(not is<int>(variant));
                                 } },
-                              { "AsCast.PolymorphicIs",
-                                [] {
+                              { "is<IsPolymorphic>",
+                                [] static noexcept {
                                     struct Foo {
                                         virtual ~Foo() {}
                                         virtual auto foo() -> void = 0;
@@ -40,34 +40,34 @@ namespace {
 
                                     auto bar = std::unique_ptr<Foo> { std::make_unique<Bar>() };
 
-                                    expects((is<Bar>(bar.get())));
-                                    expects((not is<BarFoo>(bar.get())));
+                                    expects(is<Bar>(bar.get()));
+                                    expects(not is<BarFoo>(bar.get()));
 
-                                    expects((is<Bar>(*bar)));
-                                    expects((not is<BarFoo>(*bar)));
+                                    expects(is<Bar>(*bar));
+                                    expects(not is<BarFoo>(*bar));
                                 } },
-                              { "AsCast.VariantAs",
-                                [] {
+                              { "as<std::variant>",
+                                [] static noexcept {
                                     using Variant1 = std::variant<bool, int>;
 
                                     auto variant = Variant1 { false };
 
                                     // std::variant &
                                     as<bool>(variant) = true;
-                                    expects((as<bool>(variant) == true));
+                                    expects(as<bool>(variant) == true);
 
                                     // std::variant &&
-                                    expects((as<bool>(std::move(variant)) == true));
+                                    expects(as<bool>(std::move(variant)) == true);
 
                                     const auto variant2 = Variant1 { 9 };
                                     // const std::variant &
-                                    expects((as<int>(variant2) == 9));
+                                    expects(as<int>(variant2) == 9);
 
                                     // const std::variant &&
-                                    expects((as<int>(std::move(variant2)) == 9));
+                                    expects(as<int>(std::move(variant2)) == 9);
                                 } },
-                              { "AsCast.PolymorphicAs",
-                                [] {
+                              { "as<IsPolymorphic>",
+                                [] static noexcept {
                                     struct Foo {
                                         virtual ~Foo() {}
                                         virtual auto foo() -> int = 0;
@@ -81,13 +81,21 @@ namespace {
 
                                     auto bar = std::unique_ptr<Foo> { std::make_unique<Bar>() };
 
-                                    expects((as<Bar*>(bar.get())->foo() == 1));
-                                    expects((as<Bar&>(bar.get()).foo() == 1));
-                                    expects((as<Bar*>(*bar)->foo() == 1));
-                                    expects((as<Bar&>(*bar).foo() == 1));
+                                    static_assert(IsPolymorphic<RemoveIndirectionsType<Bar*>>);
+                                    static_assert(IsPolymorphic<ElementType<PureType<Foo*>>>);
+                                    static_assert(Is<RawType<Bar*>, ElementType<PureType<Foo*>>>);
+
+                                    AsCaster<Bar*, Foo*>::operator()(bar.get());
+
+                                    expects(as<Bar*>(bar)->foo() == 1);
+                                    expects(as<Bar&>(bar).foo() == 1);
+                                    expects(as<Bar*>(bar.get())->foo() == 1);
+                                    expects(as<Bar&>(bar.get()).foo() == 1);
+                                    expects(as<Bar*>(*bar)->foo() == 1);
+                                    expects(as<Bar&>(*bar).foo() == 1);
                                 } },
-                              { "AsCast.ByteAs",
-                                [] {
+                              { "as<Byte>",
+                                [] static noexcept {
                                     struct Foo {
                                         int  a;
                                         int  b;
@@ -113,8 +121,8 @@ namespace {
                                     expects(bar_ptr->b == 3);
                                     expects(bar_ptr->c == 'x');
                                 } },
-                              { "AsCast.EnumerationAs",
-                                [] {
+                              { "as<IsEnumeration>",
+                                [] static noexcept {
                                     enum Foo : int {
                                         A = 5
                                     };
@@ -123,29 +131,29 @@ namespace {
                                     };
 
                                     auto foo = Foo::A;
-                                    expects((as<Underlying>(foo) == 5));
-                                    expects((as<Underlying>(Bar::A) == 5));
-                                    expects((as<int>(foo) == 5));
-                                    expects((as<unsigned int>(Bar::A) == 5));
-                                    expects((as<Foo>(as<int>(foo)) == Foo::A));
+                                    expects(as<Underlying>(foo) == 5);
+                                    expects(as<Underlying>(Bar::A) == 5);
+                                    expects(as<int>(foo) == 5);
+                                    expects(as<unsigned int>(Bar::A) == 5);
+                                    expects(as<Foo>(as<int>(foo)) == Foo::A);
                                 } },
-                              { "AsCast.ExplicitCastAs",
-                                [] {
+                              { "as.explicit_cast",
+                                [] static noexcept {
                                     struct Foo {};
                                     struct Bar {
                                         Bar(Foo) {};
-                                        explicit operator bool() { return true; }
+                                        operator bool() { return true; }
                                     };
 
                                     auto foo = Foo {};
 
-                                    expects((as<Bar>(Foo {})));
-                                    expects((as<Bar>(foo)));
-                                    expects((as<Bar>(std::as_const(foo))));
-                                    expects((as<Bar>(std::move(foo))));
+                                    expects(as<Bar>(Foo {}));
+                                    expects(as<Bar>(foo));
+                                    expects(as<Bar>(std::as_const(foo)));
+                                    expects(as<Bar>(std::move(foo)));
                                 } },
-                              { "AsCast.NumericAs",
-                                [] {
+                              { "as<Integers>",
+                                [] static noexcept {
                                     static_assert(IsNarrowing<int, short>);
                                     static_assert(not IsNarrowing<int, unsigned int>);
                                     static_assert(not IsNarrowing<float, unsigned int>);
@@ -153,17 +161,17 @@ namespace {
                                     static_assert(not IsNarrowing<unsigned int, float>);
 
                                     using schar = signed char;
-                                    expects((as<signed char>(127) == schar { 127 }));
-                                    expects((as<signed char>(-80) != schar { -81 }));
+                                    expects(as<signed char>(127) == schar { 127 });
+                                    expects(as<signed char>(-80) != schar { -81 });
 
-                                    expects((as<int>(8u) == 8));
-                                    expects((as<int>(1820) != 7));
+                                    expects(as<int>(8u) == 8);
+                                    expects(as<int>(1820) != 7);
 
-                                    expects((as<unsigned int>(14) == 14u));
-                                    expects((as<unsigned int>(-1) != 1));
+                                    expects(as<unsigned int>(14) == 14u);
+                                    expects(as<unsigned int>(-1) != 1);
 
-                                    expects((is(as<float>(19), 19.f)));
-                                    expects((is(as<float>(-5), -5.)));
+                                    expects(is(as<float>(19), 19.f));
+                                    expects(is(as<float>(-5), -5.));
                                 } },
                           } };
-}
+} // namespace
