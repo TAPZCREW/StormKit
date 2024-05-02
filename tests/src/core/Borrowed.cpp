7c3e9eb10ff4d4b1f237316cf3ddb211bf63cc77
Arthur LAURENT <arthur.laurent4@gmail.com>
2024-05-02 03:09:02 +0200

    [Core] rename NakedRef TU to Borrowed and fix Borrowed tests

---
+++ tests/src/core/Borrowed.cpp
@@ -0,0 +1,122 @@
+// Copyright (C) 2024 Arthur LAURENT <arthur.laurent4@gmail.com>
+// This file is subject to the license terms in the LICENSE file
+// found in the top-level of this distribution
+
+import std;
+
+import stormkit.Core;
+
+import Test;
+
+using namespace stormkit::core;
+
+#define expects(x) test::expects(x, #x)
+
+namespace {
+    auto _ = test::TestSuite { "Core.Borrowed",
+                               {
+                                   { "borrows<std::vector>.all_ref",
+                                     [] static noexcept {
+                                         auto a = 0;
+                                         auto b = 1;
+                                         auto c = 2;
+                                         auto d = 3;
+                                         auto e = 4;
+                                         auto f = 5;
+
+                                         auto refs = borrows<std::vector>(a, b, c, d, e, f);
+
+                                         auto i = 0;
+                                         for (auto&& ref : refs) expects(*ref == i++);
+                                     } },
+                                   { "borrows<std::array>.all_ref",
+                                     [] static noexcept {
+                                         auto a = 0;
+                                         auto b = 1;
+                                         auto c = 2;
+                                         auto d = 3;
+                                         auto e = 4;
+                                         auto f = 5;
+
+                                         auto refs = borrows<std::array>(a, b, c, d, e, f);
+
+                                         auto i = 0;
+                                         for (auto&& ref : refs) expects(*ref == i++);
+                                     } },
+                                   { "borrows<default>.all_ref",
+                                     [] static noexcept {
+                                         auto a = 0;
+                                         auto b = 1;
+                                         auto c = 2;
+                                         auto d = 3;
+                                         auto e = 4;
+                                         auto f = 5;
+
+                                         auto refs = borrows(a, b, c, d, e, f);
+
+                                         auto i = 0;
+                                         for (auto&& ref : refs) expects(*ref == i++);
+                                     } },
+                                   { "borrows<std::vector>.all_ptr",
+                                     [] static noexcept {
+                                         auto a = std::make_unique<int>(0);
+                                         auto b = std::make_unique<int>(1);
+                                         auto c = std::make_unique<int>(2);
+                                         auto d = new int { 3 };
+                                         auto e = new int { 4 };
+                                         auto f = 5;
+
+                                         auto refs = borrows<std::vector>(a, b, c, d, e, &f);
+
+                                         auto i = 0;
+                                         for (auto&& ref : refs) expects(*ref == i++);
+
+                                         delete d;
+                                         delete e;
+                                     } },
+                                   { "borrows<std::array>.all_ptr",
+                                     [] static noexcept {
+                                         auto a = std::make_unique<int>(0);
+                                         auto b = std::make_unique<int>(1);
+                                         auto c = std::make_unique<int>(2);
+                                         auto d = new int { 3 };
+                                         auto e = new int { 4 };
+                                         auto f = 5;
+
+                                         auto refs = borrows<std::array>(a, b, c, d, e, &f);
+
+                                         auto i = 0;
+                                         for (auto&& ref : refs) expects(*ref == i++);
+                                     } },
+                                   { "borrows<default>.all_ptr",
+                                     [] static noexcept {
+                                         auto a = std::make_unique<int>(0);
+                                         auto b = std::make_unique<int>(1);
+                                         auto c = std::make_unique<int>(2);
+                                         auto d = new int { 3 };
+                                         auto e = new int { 4 };
+                                         auto f = 5;
+
+                                         auto refs = borrows(a, b, c, d, e, &f);
+
+                                         auto i = 0;
+                                         for (auto&& ref : refs) expects(*ref == i++);
+                                     } },
+                                   { "toBorroweds<std::set>",
+                                     [] static noexcept {
+                                         auto vec = std::vector { 1, 3, 5, 6, 9 };
+                                         auto refs = toBorroweds<std::set>(vec);
+
+                                         auto i = 0;
+                                         for (auto&& ref : refs) expects(ref == vec[i++]);
+                                     } },
+                                   { "toBorroweds<default>",
+                                     [] static noexcept {
+                                         constexpr auto vec = std::array { 1, 3, 5, 6, 9 };
+                                         auto refs = toBorroweds(vec);
+
+                                         auto i = 0;
+                                         for (auto&& ref : refs) expects(ref == vec[i++]);
+                                     } },
+                               } };
+} // namespace
