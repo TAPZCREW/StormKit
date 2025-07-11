7f34bc4716cf192fa12f77ace011c0be303b6d47
Arthur LAURENT <arthur.laurent4@gmail.com>
2025-07-11 15:20:25 +0200

    (XMake) fix compile.shaders rules not finding nzslc binary

--- xmake/rules/nzsl2spv.lua
+++ xmake/rules/nzsl2spv.lua
@@ -1,7 +1,52 @@
+-- Merge binary shaders to archivess
+rule("find_nzsl", function()
+    on_config(function(target)
+        import("core.project.project")
+        import("core.tool.toolchain")
+        import("lib.detect.find_tool")
+
+        -- on windows+asan/mingw we need run envs because of .dll dependencies which may be not part of the PATH
+        local envs
+        if is_plat("windows") then
+            local msvc = target:toolchain("msvc")
+            if msvc and msvc:check() then envs = msvc:runenvs() end
+        elseif is_plat("mingw") then
+            local mingw = target:toolchain("mingw")
+            if mingw and mingw:check() then envs = mingw:runenvs() end
+        end
+        target:data_set("nzsl_envs", envs)
+
+        -- find nzsl binaries
+        local nzsl = project.required_package("nzsl~host") or project.required_package("nzsl")
+        local nzsldir
+        if nzsl then
+            nzsldir = path.join(nzsl:installdir(), "bin")
+            local osenvs = os.getenvs()
+            envs = envs or {}
+            for env, values in pairs(nzsl:get("envs")) do
+                local flatval = path.joinenv(values)
+                local oldenv = envs[env] or osenvs[env]
+                if not oldenv or oldenv == "" then
+                    envs[env] = flatval
+                elseif not oldenv:startswith(flatval) then
+                    envs[env] = flatval .. path.envsep() .. oldenv
+                end
+            end
+        end
+
+        local nzsla = find_tool("nzsla", { version = true, paths = nzsldir, envs = envs })
+        local nzslc = find_tool("nzslc", { version = true, paths = nzsldir, envs = envs })
+
+        target:data_set("nzsla", nzsla)
+        target:data_set("nzslc", nzslc)
+        target:data_set("nzsl_runenv", envs)
+    end)
+end)
+
 -- Compile shaders to includables headers
 rule("compile.shaders", function()
     set_extensions(".nzsl", ".nzslb")
-    add_deps("@nzsl/find_nzsl")
+    add_deps("find_nzsl")
     on_config(function(target)
         local archives = {}
 
@@ -44,22 +89,19 @@ rule("compile.shaders", function()
         import("lib.detect.find_tool")
 
         local outputdir = path.join(import("core.project.config").builddir(), "shaders")
-        local nzslc = find_tool("nzslc") -- target:data("nzslc")
+        local nzslc = target:data("nzslc")
         local runenvs = target:data("nzsl_runenv")
         assert(nzslc, "nzslc not found! please install nzsl package with nzslc enabled")
 
-       local outputfile = path.join(
-            outputdir or path.directory(shaderfile),
-            path.basename(shaderfile) .. ".spv"
-        )
+        local outputfile = path.join(outputdir or path.directory(shaderfile), path.basename(shaderfile) .. ".spv")
 
         -- add commands
         batchcmds:show_progress(opt.progress, "${color.build.object}compiling.shader %s", shaderfile)
-    		local argv = { "--compile=spv", "--optimize", "--spv-version=130", "--gl-flipy" }
-    		if outputdir then
-    			batchcmds:mkdir(outputdir)
-    			table.insert(argv, "--output=" .. outputdir)
-    		end
+        local argv = { "--compile=spv", "--optimize", "--spv-version=130", "--gl-flipy" }
+        if outputdir then
+            batchcmds:mkdir(outputdir)
+            table.insert(argv, "--output=" .. outputdir)
+        end
 
         -- handle --log-format
         local kind = target:data("plugin.project.kind") or ""
