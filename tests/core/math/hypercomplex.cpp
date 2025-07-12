18fd2512bab5396b37b910947156ef606d36a1a4
Arthur LAURENT <arthur.laurent4@gmail.com>
2025-07-12 20:04:49 +0200

    (core) base files for math library

---
+++ tests/core/math/hypercomplex.cpp
@@ -0,0 +1,17 @@
+// Copyright (C) 2024 Arthur LAURENT <arthur.laurent4@gmail.com>
+// This file is subject to the license terms in the LICENSE file
+// found in the top-level of this distribution
+
+import std;
+
+import stormkit.core;
+import stormkit.test;
+
+#include <stormkit/test/test_macro.hpp>
+
+using namespace stormkit::core;
+using namespace std::literals;
+
+namespace {
+    auto _ = test::TestSuite { "core.math.hypercomplex", {} };
+} // namespace
