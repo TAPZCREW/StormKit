3d50ea56fa654ae43971dd2d50a1c718024b75d3
Arthur LAURENT <arthur.laurent4@gmail.com>
2025-07-19 07:10:09 +0200

    feat(core) implement SHMBuffer for posix and win32

--- src/core/posix/shmbuffer.cpp
+++ src/core/posix/shmbuffer.cpp
@@ -4,9 +4,11 @@
 
 module;
 
+#include <errno.h>
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 module stormkit.core;
 
@@ -16,12 +18,50 @@ namespace stormkit { inline namespace core {
     /////////////////////////////////////
     /////////////////////////////////////
     SHMBuffer::~SHMBuffer() {
+        if (m_handle) {
+            munmap(stdr::data(m_data), m_size);
+            shm_unlink(stdr::data(m_name));
+            m_handle = nullptr;
+            m_size   = 0;
+        }
     }
 
     /////////////////////////////////////
     /////////////////////////////////////
-    auto SHMBuffer::allocate_buffer(bool named) noexcept -> std::expected<void, SHMBuffer::Error> {
-        (void)named;
+    auto SHMBuffer::allocate_buffer() noexcept -> std::expected<void, std::error_code> {
+        const auto shm_access = (check_flag_bit(m_access, Access::WRITE) ? O_RDWR : O_RDONLY)
+                                | ((m_handle != nullptr) ? O_TRUNC : O_CREAT);
+
+        const auto mode = init<i32>([access = m_access](auto& mode) noexcept {
+            if (check_flag_bit(access, Access::READ)) mode |= S_IRUSR;
+            if (check_flag_bit(access, Access::WRITE)) mode |= S_IWUSR;
+        });
+
+        m_handle = std::bit_cast<void*>(iptr { shm_open(stdr::data(m_name), shm_access, mode) });
+        if (not m_handle)
+            return std::unexpected {
+                std::error_code { as<i32>(errno), std::system_category() }
+            };
+        const auto fd = narrow<i32>(std::bit_cast<iptr>(m_handle));
+
+        const auto ret = ftruncate(fd, m_size);
+        if (ret < 0)
+            return std::unexpected {
+                std::error_code { as<i32>(errno), std::system_category() }
+            };
+
+        const auto prot_access = init<i32>([access = m_access](auto& prot_access) noexcept {
+            if (check_flag_bit(access, Access::READ)) prot_access |= PROT_READ;
+            if (check_flag_bit(access, Access::WRITE)) prot_access |= PROT_WRITE;
+        });
+
+        auto buf = mmap(nullptr, m_size, prot_access, MAP_SHARED, fd, 0);
+        if (not buf)
+            return std::unexpected {
+                std::error_code { as<i32>(errno), std::system_category() }
+            };
+
+        m_data = { std::bit_cast<Byte*>(buf), m_size };
 
         return {};
     }
--- src/core/win32/shmbuffer.cpp
+++ src/core/win32/shmbuffer.cpp
@@ -4,6 +4,10 @@
 
 module;
 
+#include <windows.h>
+
+#include <WinNT.h>
+
 module stormkit.core;
 
 import :containers.shmbuffer;
@@ -12,12 +16,44 @@ namespace stormkit { inline namespace core {
     /////////////////////////////////////
     /////////////////////////////////////
     SHMBuffer::~SHMBuffer() {
+        if (m_handle) {
+            UnmapViewOfFile(stdr::data(m_data));
+            CloseHandle(std::bit_cast<HANDLE>(m_handle));
+            m_handle = nullptr;
+            m_size   = 0;
+        }
     }
 
     /////////////////////////////////////
     /////////////////////////////////////
-    auto SHMBuffer::allocate_buffer(bool named) noexcept -> std::expected<void, SHMBuffer::Error> {
-        (void)named;
+    auto SHMBuffer::allocate_buffer() noexcept -> std::expected<void, std::error_code> {
+        const auto page_access = (check_flag_bit(m_access, Access::WRITE) ? PAGE_READWRITE
+                                                                          : PAGE_READONLY);
+
+        // TODO handle reallocation
+        m_handle = ::CreateFileMapping(INVALID_HANDLE_VALUE,
+                                       nullptr,
+                                       page_access,
+                                       0,
+                                       as<DWORD>(m_size),
+                                       stdr::data(m_name));
+        if (m_handle == nullptr)
+            return std::unexpected {
+                std::error_code { as<i32>(GetLastError()), std::system_category() }
+            };
+
+        const auto file_access = init<u32>([access = m_access](auto& file_access) noexcept {
+            if (check_flag_bit(access, Access::READ)) file_access |= FILE_MAP_READ;
+            if (check_flag_bit(access, Access::WRITE)) file_access |= FILE_MAP_WRITE;
+        });
+
+        auto buf = ::MapViewOfFile(m_handle, file_access, 0, 0, as<DWORD>(m_size));
+        if (buf == nullptr)
+            return std::unexpected {
+                std::error_code { as<i32>(GetLastError()), std::system_category() }
+            };
+
+        m_data = { std::bit_cast<Byte*>(buf), m_size };
 
         return {};
     }
