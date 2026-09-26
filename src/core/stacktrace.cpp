11e819f62a99f2bc1deb09c90a836a1c15d57b59
Arthur LAURENT <arthur.laurent4@gmail.com>
2026-09-26 18:46:05 +0200

    .

--- src/core/stacktrace.cpp
+++ src/core/stacktrace.cpp
@@ -136,7 +136,7 @@ namespace stormkit { inline namespace core {
         auto       frames = array_view<void*> { frames_raw }.subspan(count);
         const auto syms   = array_view<char*> { backtrace_symbols(stdr::data(frames), count), as<usize>(count) }
                             | stdv::transform([](char* str) static noexcept { return string_view { str }; })
-                            | stdr::to<dynarray>();
+                            | stdr::to<dynarray<char*>>();
 
         std::println("{}", syms);
 #else
